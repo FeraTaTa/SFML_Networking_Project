486b73928c1874dc981c73ae178ad727cc1c888d
FeraTaTa <fhathaf@gmail.com>
2019-11-17 23:05:44 +0000

    Get green circle moving

--- SFML_Networking_Project/Game.cpp
+++ SFML_Networking_Project/Game.cpp
@@ -6,8 +6,28 @@
 void Game::initWindow()
 {
 	//creates an SFML window using options from window.ini file.
-	this->window = new sf::RenderWindow(sf::VideoMode(200, 200), "SFML works!");
-	std::cout << "init window render";
+	std::ifstream ifs("Config/window.ini");
+	sf::VideoMode window_bounds(800,600);
+	std::string title = "None";
+	unsigned framerate_limit = 120;
+	bool vertical_sync_enabled = false;
+
+	if (ifs.is_open()) {
+		std::getline(ifs, title);
+		ifs >> window_bounds.width >> window_bounds.height;
+		ifs >> framerate_limit;
+		ifs >> vertical_sync_enabled;
+	}
+	ifs.close();
+	
+	this->window = new sf::RenderWindow(window_bounds, title);
+	this->window->setFramerateLimit(framerate_limit);
+	this->window->setVerticalSyncEnabled(vertical_sync_enabled);
+}
+
+void Game::initStates()
+{
+	this->states.push(new GameState(this->window));
 }
 
 //constructors/destructors
@@ -16,25 +36,33 @@ void Game::initWindow()
 Game::Game()
 {
 	this->initWindow();
+	this->initStates();
 
 
+	
 }
 
 
 Game::~Game()
 {
 	delete this->window;
+
+	while (!this->states.empty()) {
+		delete this->states.top();
+		this->states.pop();
+	}
 }
 
+//Functions
 void Game::updateDT()
 {
 	//updates the dt variable with the time it takes to update and render one frame
 	this->dt = this->dtClock.restart().asSeconds();
-	system("cls");
+	//system("cls");
 	std::cout << this->dt << std::endl;
 }
 
-//Functions
+//Captures event for closing window
 void Game::updateSFMLEvents()
 {
 	while (this->window->pollEvent(sfEvent))
@@ -47,23 +75,39 @@ void Game::updateSFMLEvents()
 void Game::update()
 {
 	this->updateSFMLEvents();
+	mousePos = (sf::Vector2f)sf::Mouse::getPosition(*(this->window));
+	listOfShapes[0].setPosition(mousePos.x-listOfShapes[0].getRadius(), mousePos.y - listOfShapes[0].getRadius());
+	std::cout << "position set to: x" << mousePos.x << " y" << mousePos.y;
+	//if (!this->states.empty()) {
+	//	this->states.top()->update(this->dt);
 
+	//	/*if (this->states.top()->getQuit()) {
+	//		delete this->states.top();
+	//		this->states.pop();
+	//	}*/
+	//}
 }
 
 void Game::render()
 {
 	this->window->clear();
-
+	
 	//render items
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Green);
-	this->window->draw(shape);
+	if (!this->states.empty()) {
+		this->states.top()->render();
+	}
+
+	this->window->draw(listOfShapes[0]);
 
 	this->window->display();
 }
 
 void Game::run()
 {
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Green);
+	listOfShapes.push_back(shape);
+
 	while (this->window->isOpen())
 	{
 		this->updateDT();
--- SFML_Networking_Project/Game.h
+++ SFML_Networking_Project/Game.h
@@ -1,15 +1,7 @@
 #ifndef GAME_H
 #define GAME_H
 
-#include <iostream>
-#include <ctime>
-#include <stdlib.h>
-
-#include <SFML/System.hpp>
-#include <SFML/Window.hpp>
-#include <SFML/Graphics.hpp>
-#include <SFML/Audio.hpp>
-#include <SFML/Network.hpp>
+#include "GameState.h"
 
 class Game
 {
@@ -20,9 +12,15 @@ private:
 	
 	sf::Clock dtClock;
 	float dt;
+	sf::Vector2f mousePos;
+	std::vector<sf::CircleShape> listOfShapes;
+	
 
+
+	std::stack<State*> states;
 	//initialization
 	void initWindow();
+	void initStates();
 
 public:
 	//constructor/destructor
